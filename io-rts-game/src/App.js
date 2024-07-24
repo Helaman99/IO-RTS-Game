cc094471be63c3c13070dc60322ccfc5acca23d2
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2024-07-24 14:53:35 -0700

    Adding the map to the app.

--- io-rts-game/src/App.js
+++ io-rts-game/src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css';
+import GameManager from './GameManager';
 
 function App() {
   return (
-    <div className="App">
-        <h1>IO RTS GAME</h1>
+    <div className = "App">
+      <h1>IO RTS GAME</h1>
+      <GameManager />
     </div>
   );
 }
