8191331de99deb6afb886a53831a611096dc7f4c
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2024-07-25 05:25:45 -0700

    Basic template for sidebar
    Just for placement and feel, not functional yet.

--- io-rts-game/src/App.js
+++ io-rts-game/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import GameManager from './GameManager';
 import ScrollZone from './ScrollZone';
+import SideBar from './SideBar';
+import GameManager from './GameManager';
 
 function App() {
   return (
     <div className = "App">
         <ScrollZone />
+        <SideBar />
         <GameManager />
     </div>
   );
---
+++ io-rts-game/src/SideBar.js
@@ -0,0 +1,17 @@
+export default function SideBar() {
+    return (
+        <div className = "side-bar">
+            <div className = "menu">
+                
+                <div id = "details">
+                    These are the details
+                </div>
+
+                <div id = "options">
+                    These are the options
+                </div>
+
+            </div>
+        </div>
+    );
+}
