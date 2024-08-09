88b351182dc9d715ab1956078a4b4155eaaf565c
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2024-08-09 12:28:01 -0700

    Unit and Building Classes

---
+++ io-rts-game/src/Buildings.js
@@ -0,0 +1,9 @@
+class Building {
+    constructor (name, health, canAttack, damage, buildableUnits) {
+        this.name = name;
+        this.health = health;
+        this.canAttack = canAttack;
+        this.damage = damage;
+        this.buildableUnits = buildableUnits;
+    }
+}
---
+++ io-rts-game/src/Units.js
@@ -0,0 +1,10 @@
+class Unit {
+    constructor (name, health, canAttack, damage, range, moveSpeed) {
+        this.name = name;
+        this.health = health;
+        this.canAttack = canAttack;
+        this.damage = damage;
+        this.range = range;
+        this.moveSpeed = moveSpeed;
+    }
+}
